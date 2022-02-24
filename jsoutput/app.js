11038331c1e28d03bca79d3d74bb65056a98d55b
robbridges <robbridges@users.noreply.github.com>
2022-02-23 19:21:14 -0800

    Removed old non tracked file cash

--- jsoutput/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var express = require('express');
-require('./db/mongoose');
-var app = express();
-var PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
-    console.log("app listening on " + PORT);
-});
--- jsoutput/db/mongoose.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var mongoose = require('mongoose');
-var dataBaseUrl = process.env.MONGODB_URL;
-mongoose.connect(dataBaseUrl, {});
