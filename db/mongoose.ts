f37718ad188f72105074b643bd628be8cb9e3d8a
robbridges <robbridges@users.noreply.github.com>
2022-02-23 19:19:51 -0800

    Ignored JS build files, got rid of test import

--- db/mongoose.ts
+++ db/mongoose.ts
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const feedback = require('../models/feedback');
-
 const dataBaseUrl: string = process.env.MONGODB_URL as string
 
 mongoose.connect(dataBaseUrl, {});
--- jsoutput/db/mongoose.js
+++ jsoutput/db/mongoose.js
@@ -1,5 +1,4 @@
 "use strict";
 var mongoose = require('mongoose');
-var feedback = require('../models/feedback');
 var dataBaseUrl = process.env.MONGODB_URL;
 mongoose.connect(dataBaseUrl, {});
